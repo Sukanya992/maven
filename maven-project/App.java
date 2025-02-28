c4bcb0f00fa53ce0023073ea9d226c11d6c40e7f
Sukanya992 <sukanya09200206@gmail.com>
2025-02-28 12:54:25 +0530

    Delete maven-project directory

--- maven-project/App.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/example/App.java
-package com.example;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
-
